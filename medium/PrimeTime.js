2ff159b4542f541a14c9c4a029e2b82ce2665958
Cory Asato <czasato@gmail.com>
2014-07-29 18:48:42 -1000

    woops

--- medium/PrimeTime.js
+++ medium/PrimeTime.js
@@ -1,13 +1,16 @@
 /*
-  Converts num's base (radix) to binary.  Modulus 10 will result in either 1 or 0, the former
-  being a Prime Number.
-
   NOTE: "A Prime Number is a natural number greater than 1 that has no positive divisors other 
          than 1 and itself."
 */
 
 function PrimeTime(num) {
-  return num === 2 || num.toString(2) % 10 === 1 ? true : false;
+  if (num % 2 === 0) return false;
+    for(var i = 3; i <= Math.sqrt(num); i = i + 2) {
+      if(num % i === 0) {
+        return false;
+      }
+    }
+    return true;
 }
 
 
